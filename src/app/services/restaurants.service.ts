0fa02d85981d4577de158ac8011af8c8cd703f19
Ilan-R <Ilan-R@users.noreply.github.com>
2019-12-03 18:28:26 +0200

    url

--- src/app/services/restaurants.service.ts
+++ src/app/services/restaurants.service.ts
@@ -9,8 +9,8 @@ import { Restaurant } from '../interfaces/restaurant';
   providedIn: 'root'
 })
 export class RestaurantsService {
-  private baseUrl = 'http://localhost:3000/api/';
- // private baseUrl ='http://ec2-63-32-104-65.eu-west-1.compute.amazonaws.com/api/';
+ // private baseUrl = 'http://localhost:3000/api/';
+  private baseUrl = 'http://ec2-63-32-104-65.eu-west-1.compute.amazonaws.com/api/';
   private rests = 'restaurants/';
   private httpOptions = {
     headers: new HttpHeaders({ 'Content-Type': 'application/json' })
